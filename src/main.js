2eda21959aa2619f3b87e8448879bbf0e2ef5ee8
霍 <huo8008@126.com>
2019-01-24 16:15:47 +0800

    修改文件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// 导出一个工厂函数，用于创建新的
+// 应用程序、router 和 store 实例
+export function createApp () {
+  const app = new Vue({
+    // 根实例简单的渲染应用程序组件。
+    render: h => h(App)
+  })
+  return { app }
+}
